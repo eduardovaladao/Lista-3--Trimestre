145b727b0234875b9e53eee26d02d456c69b0310
eduardovaladao <eduardovaladao2006@gmail.com>
2023-12-09 11:12:26 -0300

    finally

--- exc2/script.js
+++ exc2/script.js
@@ -1,22 +1,27 @@
 $(document).ready(() => {
 
-    $('#consultar').after('<br><span id="warning"></span>');
+    var warning = $('#warning');
+
+    //$('#consultar').after('<br><span id="warning"></span>');
     $('#consultar').click(() => {
         consultarRegioes();
     });
 
     function consultarRegioes() {
         var mensagem = "";
+        warning.hide();
+        warning.removeClass();
+
         if ($('#regioes').val() != "") {
             $.getJSON('https://servicodados.ibge.gov.br/api/v1/localidades/estados/', (estados, status) => {
-                console.log(estados);
-                console.log($('#regioes').val());
+                //console.log(estados);
+                //console.log($('#regioes').val());
 
                 $('table').html("");
 
                 var tableHTML = "";
                 var tr = "";
-
+                
                 for (let i = 0; i < estados.length; i++) {
 
                     if (estados[i].regiao.nome == $('#regioes').val()) {
@@ -26,33 +31,19 @@ $(document).ready(() => {
                         tableHTML += tr;
                     }
                 }
-
                 $('#Guilherme-e-Santiago').html(tableHTML);
-
-                $('#Guilherme-e-Santiago').css(
-                    { display: "block" }
-                )
+                $('#Guilherme-e-Santiago').css({ display: "block" });
 
                 mensagem = "Tudo certo!!!";
+                warning.addClass("alert alert-success");
+                warning.html(mensagem);
 
-                $('#warning').html(mensagem);
-                $('#warning').css(
-                    {
-                        backgroundColor: "green",
-                        width: "100%"
-                    }
-                );
             });
         } else {
             mensagem = "Você não selecionou uma região!";
-
-            $('#warning').html(mensagem);
-            $('#warning').css(
-                {
-                    backgroundColor: "red",
-                    width: "100%"
-                }
-            ); 
+            warning.addClass("alert alert-danger");
+            warning.html(mensagem);
         }
+        warning.show("slow");
     }
 });
